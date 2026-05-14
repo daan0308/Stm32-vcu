1966fc10e957fa3fddca8fa893be589f6534368e
Allard <allard@mediabuyingsystems.com>
2026-05-14 20:44:27 +0200

    updated version and fixed null chars

--- include/param_prj.h
+++ include/param_prj.h
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define VER 2.30.A.DAALPO.2
+#define VER 2.30.A.DAALPO.2.1
 
 
 /* Entries must be ordered as follows:
@@ -166,8 +166,8 @@
     VALUE_ENTRY(power,         "kW",                2011 ) \
     VALUE_ENTRY(idc,           "A",                 2012 ) \
     VALUE_ENTRY(KWh,           "kwh",               2013 ) \
-    VALUE_ENTRY(range,         "km",                2109 ) \
-    VALUE_ENTRY(consumption,   "Wh/km",             2110 ) \
+    VALUE_ENTRY(range,         "km",                2110 ) \
+    VALUE_ENTRY(consumption,   "Wh/km",             2111 ) \
     VALUE_ENTRY(AMPh,          "Ah",                2014 ) \
     VALUE_ENTRY(SOC,           "%",                 2015 ) \
     VALUE_ENTRY(BMS_Vmin,      "V",                 2084 ) \
@@ -249,7 +249,7 @@
     VALUE_ENTRY(VehLockSt,     ONOFF,               2100 ) \
     VALUE_ENTRY(ImdFault,      ONOFF,               2108 ) \
     VALUE_ENTRY(ImdState,      IMDSTATE,            2109 ) \
-//Next value Id: 2111
+//Next value Id: 2112
 
 //Dead params
 /*
